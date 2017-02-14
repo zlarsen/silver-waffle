42013f26958b7d79cf206dc09cc36e8f0f24c6d4
zlarsen <zlarsen@dmail.dixie.edu>
2017-02-13 18:15:03 -0700

    Adds the logic for cursors and saving

--- game.js
+++ game.js
@@ -1,22 +1,94 @@
-
-//bits is our variable to store how many bits the person has earned. We need to store this in a cookie eventually
-var bits = 0
+var bits = 0;
+var cursors = 0;
 
 //this jQuery function looks for the div identified by the #clickBox id and waits for a click on it. Known as a click handler.
 $('#clickBox').click(function () {
+    
     //every click we add a bit and update the score span to reflect the new info
-    bits += 1;
-    $('#score').text(calcScoreText(bits)); 
-})
+    bitClick(1);
+});
 
-function calcScoreText(score) {
+$('.buyCursor').click(function () {
+    buyCursor();
+});
+
+$('#save').click(function () {
+    save();
+});
+
+$('#deleteSave').click(function () {
+    deleteSave();
+});
+
+function bitClick(number){
+    bits = bits + number;
+    $('#score').text(calcAndSaveScore(bits)); 
+};
+
+function calcAndSaveScore(score) {
+    return setScoreText(score);
+}
+
+
+function setScoreText(score) {
     var scoreText = '';
     if (score < 8) {
         scoreText = 'Bits: '+score;
-    } else if (score >= 8 && score < 8192) {
-        scoreText = 'Bytes: '+(score/8);
-    } else if (score >= 8192 && score < 8388608) {
-        scoreText = 'Kilobytes: '+(score/8000);
+    } else {
+        scoreText = formatBytes(score/8);
     }
     return scoreText;
 }
+
+function formatBytes(bytes,decimals) {
+   if(bytes == 0) return '0 Bytes';
+   var k = 1024,
+       dm = decimals + 1 || 3,
+       sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+       i = Math.floor(Math.log(bytes) / Math.log(k));
+   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
+    if(bits >= cursorCost){                                   //checks that the player can afford the cursor
+        cursors = cursors + 1;                                   //increases number of cursors
+        bits = bits - cursorCost;                          //removes the bits spent
+        $('#cursors').text('Cursors: '+cursors);//updates the number of cursors for the user
+        $('#score').text(calcAndSaveScore(bits));   //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+    $('#cursorsCost').text('Cursor Price: '+formatBytes(nextCost/8));  //updates the cursor cost for the user
+};
+
+function load() {
+    var savegame = JSON.parse(localStorage.getItem("save"));
+    if (savegame) {
+        if (typeof savegame.bits !== "undefined") bits = savegame.bits;
+        if (typeof savegame.cursors !== "undefined") cursors = savegame.cursors;
+    }
+}
+
+function save() {
+    var save = {
+        bits: bits,
+        cursors: cursors,
+    }
+    localStorage.setItem("save",JSON.stringify(save));
+}
+
+function deleteSave() {
+    localStorage.removeItem("save")
+}
+
+$(document).ready(function() {
+    load();
+    var nextCursorCost = Math.floor(10 * Math.pow(1.1,cursors));
+    $('#score').text(calcAndSaveScore(bits));
+    $('#cursors').text('Cursors: '+cursors);
+    $('#cursorsCost').text('Cursor Price: '+formatBytes(nextCursorCost/8));
+});
+
+var gameLoop = setInterval(function() {
+    bitClick(cursors);
+}, 1000);
